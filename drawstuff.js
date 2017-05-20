0e6973367bbba3b1aade0a0ee294e5f356d7223e
bwatson <bwatson@ncsu.edu>
2017-05-20 10:11:27 -0400

    update shell from github.com

--- drawstuff.js
+++ drawstuff.js
@@ -1,6 +1,6 @@
 /* classes */ 
 
-// Color class
+// Color constructor
 class Color {
     constructor(r,g,b,a) {
         try {
@@ -32,7 +32,7 @@ class Color {
             else {
                 this.r = r; this.g = g; this.b = b; this.a = a; 
             }
-        } // end try
+        } // end throw
         
         catch (e) {
             console.log(e);
@@ -40,192 +40,9 @@ class Color {
     } // end Color change method
 } // end color class
 
-// Vector class
-class Vector { 
-    constructor(x,y,z) {
-        this.set(x,y,z);
-    } // end constructor
-    
-    // sets the components of a vector
-    set(x,y,z) {
-        try {
-            if ((typeof(x) !== "number") || (typeof(y) !== "number") || (typeof(z) !== "number"))
-                throw "vector component not a number";
-            else
-                this.x = x; this.y = y; this.z = z; 
-        } // end try
-        
-        catch(e) {
-            console.log(e);
-        }
-    } // end vector set
-    
-    // copy the passed vector into this one
-    copy(v) {
-        try {
-            if (!(v instanceof Vector))
-                throw "Vector.copy: non-vector parameter";
-            else
-                this.x = v.x; this.y = v.y; this.z = v.z;
-        } // end try
-        
-        catch(e) {
-            console.log(e);
-        }
-    }
-    
-    toConsole(prefix) {
-        console.log(prefix+"["+this.x+","+this.y+","+this.z+"]");
-    } // end to console
-    
-    // static dot method
-    static dot(v1,v2) {
-        try {
-            if (!(v1 instanceof Vector) || !(v2 instanceof Vector))
-                throw "Vector.dot: non-vector parameter";
-            else
-                return(v1.x*v2.x + v1.y*v2.y + v1.z*v2.z);
-        } // end try
-        
-        catch(e) {
-            console.log(e);
-            return(NaN);
-        }
-    } // end dot static method
-    
-    // static add method
-    static add(v1,v2) {
-        try {
-            if (!(v1 instanceof Vector) || !(v2 instanceof Vector))
-                throw "Vector.add: non-vector parameter";
-            else
-                return(new Vector(v1.x+v2.x,v1.y+v2.y,v1.z+v2.z));
-        } // end try
-        
-        catch(e) {
-            console.log(e);
-            return(new Vector(NaN,NaN,NaN));
-        }
-    } // end add static method
-
-    // static subtract method, v1-v2
-    static subtract(v1,v2) {
-        try {
-            if (!(v1 instanceof Vector) || !(v2 instanceof Vector))
-                throw "Vector.subtract: non-vector parameter";
-            else {
-                var v = new Vector(v1.x-v2.x,v1.y-v2.y,v1.z-v2.z);
-                //v.toConsole("Vector.subtract: ");
-                return(v);
-            }
-        } // end try
-        
-        catch(e) {
-            console.log(e);
-            return(new Vector(NaN,NaN,NaN));
-        }
-    } // end subtract static method
-
-    // static scale method
-    static scale(c,v) {
-        try {
-            if (!(typeof(c) === "number") || !(v instanceof Vector))
-                throw "Vector.scale: malformed parameter";
-            else
-                return(new Vector(c*v.x,c*v.y,c*v.z));
-        } // end try
-        
-        catch(e) {
-            console.log(e);
-            return(new Vector(NaN,NaN,NaN));
-        }
-    } // end scale static method
-} // end Vector class
-
-
 
 /* utility functions */
 
-// Solve quadratic. Return empty array if no solutions, 
-// one t value if one solution, two if two solutions.
-function solveQuad(a,b,c) {
-    var discr = b*b - 4*a*c; 
-    // console.log("a:"+a+" b:"+b+" c:"+c);
-
-    if (discr < 0) { // no solutions
-        // console.log("no roots!");
-        return([]); 
-    } else if (discr == 0) { // one solution
-        // console.log("root: "+(-b/(2*a)));
-        return([-b/(2*a)]);
-    } else { // two solutions
-        var denom = 0.5/a;
-        var term1 = -b;
-        var term2 = Math.sqrt(discr)
-        var tp = denom * (term1 + term2);
-        var tm = denom * (term1 - term2);
-        // console.log("root1:"+tp+" root2:"+tm);
-        if (tm < tp)
-            return([tm,tp]);
-        else
-            return([tp,tm]);
-    } 
-} // end solveQuad
-    
-// ray sphere intersection
-// if no intersect, return NaN
-// if intersect, return xyz vector and t value
-function raySphereIntersect(ray,sphere) {
-    try {
-        if (!(ray instanceof Array) || !(sphere instanceof Array))
-            throw "RaySphereIntersect: ray or sphere are not arrays";
-        else if ((ray.length != 2) || (sphere.length != 4))
-            throw "RaySphereIntersect: badly formatted ray or sphere";
-        else { // valid params
-            var a = Vector.dot(ray[1],ray[1]); // dot(D,D)
-            var origMctr = Vector.subtract(ray[0],new Vector(sphere[0],sphere[1],sphere[2])); // E-C
-            var b = 2 * Vector.dot(ray[1],origMctr); // 2 * dot(D,E-C)
-            var c = Vector.dot(origMctr,origMctr) - sphere[3]*sphere[3]; // dot(E-C,E-C) - r^2
-            //ray[0].toConsole("ray.orig: (");
-            //ray[1].toConsole("ray.dir: [");
-            //console.log("a:"+a+" b:"+b+" c:"+c);
-        
-            var qsolve = solveQuad(a,b,c);
-            if (qsolve.length == 0) 
-                throw "no intersection";
-            else if (qsolve.length == 1) { 
-                if (qsolve[0] < 1)
-                    throw "intersection in front of window";
-                else {
-                    var isect = Vector.add(ray[0],Vector.scale(qsolve[0],ray[1]));
-                    //console.log("t: "+qsolve[0]);
-                    //isect.toConsole("intersection: ");
-                    return({"xyz": isect,"t": qsolve[0]});  
-                } // one intersection behind window
-            } else if (qsolve[0] < 1) {
-                if (qsolve[1] < 1)
-                    throw "intersections in front of window";
-                else { 
-                    var isect = Vector.add(ray[0],Vector.scale(qsolve[1],ray[1]));
-                    //console.log("t2: "+qsolve[1]);
-                    //isect.toConsole("intersection: ");
-                    return({"xyz": isect,"t": qsolve[1]});  
-                } // one intersect in front, one behind
-            } else {
-                var isect = Vector.add(ray[0],Vector.scale(qsolve[0],ray[1]));
-                //console.log("t1: "+qsolve[0]);
-                //isect.toConsole("intersection: ");
-                return({"xyz": isect,"t": qsolve[0]});  
-            } // both intersects behind
-        } // end if valid params
-    } // end try
-
-    catch(e) {
-        //console.log(e);
-        return(NaN);
-    }
-} // end raySphereIntersect
-    
 // draw a pixel at x,y using color
 function drawPixel(imagedata,x,y,color) {
     try {
@@ -272,7 +89,7 @@ function drawRandPixels(context) {
 // get the input spheres from the standard class URL
 function getInputSpheres() {
     const INPUT_SPHERES_URL = 
-        "https://pages.github.ncsu.edu/bwatson/introcg-prog1/spheres.json";
+        "https://ncsucgclass.github.io/prog1/spheres.json";
         
     // load the spheres file
     var httpReq = new XMLHttpRequest(); // a new http request
@@ -344,6 +161,7 @@ function drawRandPixelsInInputSpheres(context) {
 function drawInputSpheresUsingArcs(context) {
     var inputSpheres = getInputSpheres();
     
+    
     if (inputSpheres != String.null) { 
         var c = new Color(0,0,0,0); // the color at the pixel: black
         var w = context.canvas.width;
@@ -372,59 +190,6 @@ function drawInputSpheresUsingArcs(context) {
     } // end if spheres found
 } // end draw input spheres
 
-// use ray casting with spheres to get pixel colors
-function rayCastSpheres(context) {
-    var inputSpheres = getInputSpheres();
-    var w = context.canvas.width;
-    var h = context.canvas.height;
-    var imagedata = context.createImageData(w,h);
-
-    if (inputSpheres != String.null) { 
-        var x = 0; var y = 0; // pixel coord init
-        var c = new Color(0,0,0,0); // init the sphere color
-        var n = inputSpheres.length; // the number of spheres
-        var sphere = []; // the current sphere
-        var Eye = new Vector(0.5,0.5,-0.5); // set the eye position
-        var Dir = new Vector(0,0,0); // init the ray direction
-        var closestT = Number.MAX_VALUE; // init the closest t value
-        var isect = {}; // init the intersection
-        const WIN_Z = 0;
-        const WIN_LEFT = 0; const WIN_RIGHT = 1;
-        const WIN_BOTTOM = 0; const WIN_TOP = 1; 
-        //console.log("number of spheres: " + n);
-
-        // Loop over the pixels and spheres, intersecting them
-        var wx = WIN_LEFT; // init world pixel xcoord
-        var wxd = (WIN_RIGHT-WIN_LEFT) * 1/(w-1); // world pixel x differential
-        var wy = WIN_TOP; // init world pixel ycoord
-        var wyd = (WIN_BOTTOM-WIN_TOP) * 1/(h-1); // world pixel y differential
-        for (y=0; y<h; y++) {
-            wx = WIN_LEFT; // init w
-            for (x=0; x<h; x++) {
-                closestT = Number.MAX_VALUE; // no closest t for this pixel
-                c.change(0,0,0,255); // set pixel to background color
-                Dir.copy(Vector.subtract(new Vector(wx,wy,WIN_Z),Eye)); // set ray direction
-                //Dir.toConsole("Dir: ");
-                for (var s=0; s<n; s++) {
-                    sphere = [inputSpheres[s].x,inputSpheres[s].y,inputSpheres[s].z,inputSpheres[s].r];
-                    isect = raySphereIntersect([Eye,Dir],sphere); 
-                    if (isect != NaN) // there is an intersect
-                        if (isect.t < closestT) { // it is the closest yet
-                            closestT = isect.t; // record closest t yet
-                            c.change(inputSpheres[s].diffuse[0]*255,inputSpheres[s].diffuse[1]*255,
-                                inputSpheres[s].diffuse[2]*255,255);
-                        } // end if closest yet
-                } // end for spheres
-                drawPixel(imagedata,x,y,c);
-                wx += wxd; 
-                //console.log(""); // blank per pixel
-            } // end for x
-            wy += wyd; 
-        } // end for y
-        context.putImageData(imagedata, 0, 0);
-    } // end if spheres found
-} // end ray cast spheres
-
 
 /* main -- here is where execution begins after window load */
 
@@ -438,11 +203,9 @@ function main() {
     //drawRandPixels(context);
       // shows how to draw pixels
     
-    //drawRandPixelsInInputSpheres(context);
+    drawRandPixelsInInputSpheres(context);
       // shows how to draw pixels and read input file
       
     //drawInputSpheresUsingArcs(context);
       // shows how to read input file, but not how to draw pixels
-      
-    rayCastSpheres(context); 
 }
