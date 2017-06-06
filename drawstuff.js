934444c6e60a338315bcda2908f8bcfb32e32a56
bwatson <bwatson@ncsu.edu>
2017-06-06 15:54:56 -0400

    debug

--- drawstuff.js
+++ drawstuff.js
@@ -104,8 +104,8 @@ class Polygon {
                 // later add convexity test
             else {
                 var vBegin = this.xArray.length - 1; // begin vertex is last poly vertex
-                var p1XArray = [this.xArray[this.xArray.length]]; // x coords of new poly 1
-                var p1YArray = [this.yArray[this.yArray.length]]; // y coords of new poly 1
+                var p1XArray = [this.xArray[this.xArray.length-1]]; // x coords of new poly 1
+                var p1YArray = [this.yArray[this.yArray.length-1]]; // y coords of new poly 1
                 var p2XArray = [], p2YArray = []; // vertices of new poly 2
                 var currXArray = p1XArray, currYArray = p1YArray; // which new poly we add to
                 var foundIsect1 = false; // if first intersection found
@@ -113,7 +113,7 @@ class Polygon {
                 var isectPoint; // the intersection point
                     
                     // move through poly edges, test for intersections, build two new polys
-                for (var e=0; e<this.xArray.length; e++) { 
+                for (var e=0; e<this.xArray.length-1; e++) { 
                     isectPoint = findIntersect(this,vBegin,e);
                     if (isectPoint == null)
                         console.log("no intersection");
@@ -137,7 +137,7 @@ class Polygon {
                 console.log(p1XArray.toString());
                 console.log(p2XArray.toString());
                 
-                if (p2XArray == []) { // no split
+                if (p2XArray.length == 0) { // no split
                     console.log("There was no split");
                     return([]);
                 } else {
