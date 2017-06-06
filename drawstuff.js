5352eff3f8d2b6a7bea777555a4a573cf6ada635
bwatson <bwatson@ncsu.edu>
2017-06-06 16:23:05 -0400

    test

--- drawstuff.js
+++ drawstuff.js
@@ -394,7 +394,7 @@ function main() {
     //console.log(poly.area());
     
     // split the polygon
-    var splitResult = poly.split(1,-100,2000);
+    var splitResult = poly.split(1,-10,200);
     if (splitResult.length == 0)
         console.log("No split.");
     else {
