01d90d016cb9d589cfa07a496dbb39f897a1b1c1
bwatson <bwatson@ncsu.edu>
2017-06-10 23:46:26 -0400

    debug

--- drawstuff.js
+++ drawstuff.js
@@ -122,8 +122,8 @@ class Polygon {
                         console.log(isectPoint.x +" "+ isectPoint.y);
                     if (isectPoint !== null) { // edge intersects line
                         if (!foundIsect1) { // found first intersect
-                            if (   (isectPoint[e].x !== p1XArray[e].x) 
-                                || (isectPoint[e].y !== p1YArray[e].y)) {
+                            if (   (isectPoint.x !== this.xArray[e].x) 
+                                || (isectPoint.y !== this.yArray[e].y)) {
                                 p1XArray.push(isectPoint.x); p1YArray.push(isectPoint.y);
                             } // end if intersect is vertex
                             p2XArray.push(isectPoint.x); p2YArray.push(isectPoint.y);
@@ -131,8 +131,8 @@ class Polygon {
                             currXArray = p2XArray; currYArray = p2YArray;
                         } else { // found second intersect
                             p1XArray.push(isectPoint.x); p1YArray.push(isectPoint.y);
-                            if (   (isectPoint[e].x !== p2XArray[e].x) 
-                                || (isectPoint[e].y !== p2YArray[e].y)) {
+                            if (   (isectPoint[e].x !== this.xArray[e].x) 
+                                || (isectPoint[e].y !== this.yArray[e].y)) {
                                 p2XArray.push(isectPoint.x); p2YArray.push(isectPoint.y);
                             } // end if intersect is vertex
                             foundIsect2 = true; 
