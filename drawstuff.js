22968268523e0710047fa7284a339bb9d6600f09
bwatson <bwatson@ncsu.edu>
2017-07-11 12:00:53 -0400

    test

--- drawstuff.js
+++ drawstuff.js
@@ -486,7 +486,7 @@ function main() {
     var poly = new Polygon(new Array(-150,0,150),new Array(-150,150,-150)); 
     
     // draw the polygon
-    poly.draw(context,w/2,h/2,1,-1);
+    // poly.draw(context,w/2,h/2,1,-1);
     // console.log(poly.area());
     
     // split the polygon
