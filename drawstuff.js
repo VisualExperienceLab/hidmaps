c9f6cc65deef1092231b8f2bdc7924e57c593e44
bwatson <bwatson@ncsu.edu>
2017-07-12 16:44:34 -0400

    test

--- drawstuff.js
+++ drawstuff.js
@@ -272,7 +272,6 @@ class Polygon {
                     
                     do { 
                         stepCoord += stepDir; depCoord += (stepDir * depDelta);
-                        console.log("
                         console.log("Testing in edge split at: "+(stepInY?depCoord:stepCoord)+" "+(stepInY?stepCoord:depCoord));
                         foundSplitPixel = (stepAreaLess(this) !== endAreaLess);
                         exitedEdge = (stepDir !== Math.sign(stepArray[beginV] - stepCoord));
