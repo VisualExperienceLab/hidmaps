4a3374bbf763dcf8b96b3b5664932c6c1da3daa9
bwatson <bwatson@ncsu.edu>
2017-08-06 01:24:40 -0400

    octagon 22.5 degs

--- drawstuff.js
+++ drawstuff.js
@@ -566,10 +566,10 @@ function main() {
     // var splitResult = poly.splitByArea(0.5,-Math.tan(18 * Math.PI/180));
     // var splitResult = poly.splitByArea(0.5,Math.tan(54 * Math.PI/180));
     // var splitResult = poly.splitByArea(0.5,-Math.tan(54 * Math.PI/180));
-    // var splitResult = poly.splitByArea(0.5,Math.tan(60 * Math.PI/180));
-    // var splitResult = poly.splitByArea(0.5,-Math.tan(60 * Math.PI/180));
-    // var splitResult = poly.splitByArea(0.5,Math.tan(30 * Math.PI/180));
-    var splitResult = poly.splitByArea(0.5,-Math.tan(30 * Math.PI/180));
+    // var splitResult = poly.splitByArea(0.5,Math.tan(67.5 * Math.PI/180));
+    // var splitResult = poly.splitByArea(0.5,-Math.tan(67.5 * Math.PI/180));
+    // var splitResult = poly.splitByArea(0.5,Math.tan(22.5 * Math.PI/180));
+    var splitResult = poly.splitByArea(0.5,-Math.tan(22.5 * Math.PI/180));
     // var splitResult = poly.splitByArea(0.5,2);
     // var splitResult = poly.splitByArea(0.5,-2);
     // var splitResult = poly.splitByArea(0.5,0);
